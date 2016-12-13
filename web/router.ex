4e9ce911902ce0b7c9ff8ff1cd0d684e6edbb148
BartlomiejSkwira <nemo@codequest.com>
2016-12-13 11:06:28 +0100

    Add GameSessionsController for API

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,15 @@
 defmodule Kot.Router do
   use Kot.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", Kot do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api
 
-    get "/", PageController, :index
+    scope "v1", V1, as: :v1 do
+      resources "game_sessions", GameSessionsController
+    end
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Kot do
-  #   pipe_through :api
-  # end
 end
